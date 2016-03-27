d768bae3b94bea8853a746dff41c59d6e197f490
mradambeck <mr.adambeck@gmail.com>
2016-03-27 16:55:43 -0700

    FINAAALLLLYYY fixed a bug causing it to choke after a couple rounds

--- js/app.js
+++ js/app.js
@@ -6,79 +6,39 @@ function Player(playerNumber){
   this.playerName = "Player " + playerNumber;
   // assigning movement to directional keys
   this.movement = function(){
-    if (this.playerNumber === 1){
-      $doc.on("keydown", function(event){
-        var $playerOne = $('#player-one');
-        var playerPosition = $playerOne.position();
-        if (event.which === 39){
-          event.preventDefault();
-          if (playerPosition.left < 200){ //890
-                $playerOne.animate({left: playerPosition.left+5}, 75);
-              }
-              scoreBoard.checkWin(playerPosition.left, "Player One");
-        }
-        // switch(event.which) {
-        //   case 39: // right arrow
-        //     event.preventDefault();
-        //     if (playerPosition.left < 200){ //890
-        //       $playerOne.animate({left: playerPosition.left+5}, 75);
-        //     }
-        //     scoreBoard.checkWin(playerPosition.left, "Player One");
-        //     break;
-        //   case 37: // left arrow
-        //     event.preventDefault();
-        //     $playerOne.animate({left: playerPosition.left-5}, 75);
-        //     scoreBoard.checkWin(playerPosition.left, "Player One");
-        //     break;
-        //   case 38: // up arrow
-        //     event.preventDefault();
-        //     $playerOne.animate({top: 20, left: playerPosition.left+10}, 300);
-        //     scoreBoard.checkWin(playerPosition.left, "Player One");
-        //     break;
-        //   case 40: // down arrow
-        //     event.preventDefault();
-        //     $playerOne.animate({top: 430, left: playerPosition.left+10}, 300);
-        //     scoreBoard.checkWin(playerPosition.left, "Player One");
-        //     break;
-        // }
-      });
-    } else if (this.playerNumber === 2){
+    if (scoreBoard.gameOver === false){
+      if (this.playerNumber === 1){
+        $doc.on("keydown", function(event){
+          var $playerOne = $('#player-one');
+          var playerPosition = $playerOne.position();
+          if (event.which === 39){
+            event.preventDefault();
+            console.log('keypress 39');
+            if (playerPosition.left < 200){ //890
+              $playerOne.animate({left: playerPosition.left+5}, 30);
+            }
+                // console.log('playerOne-R');
+            scoreBoard.checkWin(playerPosition.left, "Player One");
+          }
+        });
+      } else if (this.playerNumber === 2){
         $doc.on("keydown", function(event){
           var $playerTwo = $('#player-two');
           var playerPosition = $playerTwo.position();
           if (event.which === 68){
             event.preventDefault();
+            console.log('keypress 68');
             if (playerPosition.left < 200){ //890
-                  $playerOne.animate({left: playerPosition.left+5}, 75);
+                  $playerTwo.animate({left: playerPosition.left+5}, 30);
                 }
+                // console.log('player2-R');
                 scoreBoard.checkWin(playerPosition.left, "Player One");
           }
-          // switch(event.which) {
-          //   case 68: // right arrow
-          //     event.preventDefault();
-          //     if (playerPosition.left < 200){
-          //       $playerTwo.animate({left: playerPosition.left+5}, 75);
-          //     }
-          //     scoreBoard.checkWin(playerPosition.left, "Player Two");
-          //     break;
-          //   case 65: // left arrow
-          //     event.preventDefault();
-          //     $playerTwo.animate({left: playerPosition.left-5}, 75);
-          //     scoreBoard.checkWin(playerPosition.left, "Player Two");
-          //     break;
-          //   case 87: // up arrow
-          //     event.preventDefault();
-          //     $playerTwo.animate({top: 20, left: playerPosition.left+10}, 300);
-          //     scoreBoard.checkWin(playerPosition.left, "Player Two");
-          //     break;
-          //   case 83: // down arrow
-          //     event.preventDefault();
-          //     $playerTwo.animate({top: 430, left: playerPosition.left+10}, 300);
-          //     scoreBoard.checkWin(playerPosition.left, "Player Two");
-          //     break;
-          // }
         });
       }
+    } else {
+      return false;
+    }
   };
 }
 
@@ -88,65 +48,44 @@ var scoreBoard = {
   p2Score: 0,
   gameOver: false,
   reset: function(){
+    console.log('reset');
     $('#player-one').animate({left: 20}, 300);
     $('#player-two').animate({left: 20}, 300);
-    this.gameOver = false;
-    gameStart();
-    $('#reset-btn').addClass('display-none');
 
+    $('#start-game').removeClass('display-none');
+    gameStart();
   },
   checkWin: function(position, winner){
+    console.log('checkWin');
     // checks position to see if at the end of the game board
-    if (position > 180 && !this.gameOver){ //885
-      alert (winner + "wins!");
+    if (position > 180 ){ //885
+      alert (winner + " wins!");
       if (winner === "Player One"){
         this.p1Score++;
         $('#p1-score').text(this.p1Score);
-      } else {
+        console.log("playerOne wins");
+      } else if (winner === "Player Two"){
         this.p2Score++;
         $('#p2-score').text(this.p2Score);
+        console.log("playerTwo wins");
       }
       this.gameOver = true;
-      console.log("p1: "+this.p1Score + "p2: " + this.p2Score + "winner: " + winner);
-      $('#reset-btn').removeClass('display-none');
-      $('#reset-btn').click(function(){
-        scoreBoard.reset();
-        $('#start-game').text("PRESS SPACEBAR TO START");
-        $('#start-game').addClass("btn-primary, btn-lg");
-        gameStart();
-      }
-    );
+      scoreBoard.reset();
     }
   }
 };
 
 var gameStart = function(){
+  // Start game with spacebar
   $doc.on("keydown", function startGame(event){
-
     if (event.keyCode === 32){
-      // Countdown to ensure everyone starts at the same time
-      $('#start-game').text("3");
-
-      // var countDown = setInterval(function(){
-      //   // clearTimeout(countDown);
-      //   $('#start-game').text(function(i,num){
-      //     if(num==="3"){
-      //       return "2";
-      //     } else if (num ==="2"){
-      //       return "1";
-      //     } else if (num ==="1"){
-      //       clearTimeout(countDown);
-      //       playerOne.movement();
-      //       playerTwo.movement();
-      //       return "Go!";
-      //     }
-      //   });
-      // }, 1000);
-
+      this.gameOver = false;
       playerOne.movement();
       playerTwo.movement();
       $('#start-game').addClass('display-none');
-
+      console.log("added display none to $startGame");
+    } else {
+      return;
     }
   });
 };
@@ -155,10 +94,11 @@ var gameStart = function(){
 var playerOne = new Player(1);
 var playerTwo = new Player(2);
 
+gameStart();
+
 $doc.ready(function() {
   console.log( "Document ready!" );
 
-// Start game with spacebar
-gameStart();
+
 
 });
