8459186e900639190547507dc3f936a030383d43
mradambeck <mr.adambeck@gmail.com>
2016-03-27 08:11:39 -0700

    scoreboard setup

--- js/app.js
+++ js/app.js
@@ -13,22 +13,22 @@ function Player(playerNumber, divID){
           case 39: // right arrow
             event.preventDefault();
             $playerOne.animate({left: playerPosition.left+5}, 75);
-            checkWin(playerPosition.left, "Player One");
+            scoreBoard.checkWin(playerPosition.left, "Player One");
             break;
           case 37: // left arrow
             event.preventDefault();
             $playerOne.animate({left: playerPosition.left-5}, 75);
-            checkWin(playerPosition.left, "Player One");
+            scoreBoard.checkWin(playerPosition.left, "Player One");
             break;
           case 38: // up arrow
             event.preventDefault();
             $playerOne.animate({top: 20, left: playerPosition.left+10}, 300);
-            checkWin(playerPosition.left, "Player One");
+            scoreBoard.checkWin(playerPosition.left, "Player One");
             break;
           case 40: // down arrow
             event.preventDefault();
             $playerOne.animate({top: 430, left: playerPosition.left+10}, 300);
-            checkWin(playerPosition.left, "Player One");
+            scoreBoard.checkWin(playerPosition.left, "Player One");
             break;
         }
       });
@@ -40,32 +40,45 @@ function Player(playerNumber, divID){
           case 68: // right arrow
             event.preventDefault();
             $playerTwo.animate({left: playerPosition.left+5}, 75);
-            checkWin(playerPosition.left, "Player Two");
+            scoreBoard.checkWin(playerPosition.left, "Player Two");
             break;
           case 65: // left arrow
             event.preventDefault();
             $playerTwo.animate({left: playerPosition.left-5}, 75);
-            checkWin(playerPosition.left, "Player Two");
+            scoreBoard.checkWin(playerPosition.left, "Player Two");
             break;
           case 87: // up arrow
             event.preventDefault();
             $playerTwo.animate({top: 20, left: playerPosition.left+10}, 300);
-            checkWin(playerPosition.left, "Player Two");
+            scoreBoard.checkWin(playerPosition.left, "Player Two");
             break;
           case 83: // down arrow
             event.preventDefault();
             $playerTwo.animate({top: 430, left: playerPosition.left+10}, 300);
-            checkWin(playerPosition.left, "Player Two");
+            scoreBoard.checkWin(playerPosition.left, "Player Two");
             break;
         }
       });
   };
 }
 
-var checkWin = function(position, winner){
-  if (position > 885){
-    alert (winner + " wins!");
-    console.log(position, winner);
+var scoreBoard = {
+  p1Score: 0,
+  p2Score: 0,
+  winner: false,
+  checkWin: function(position, winner){
+    if (position > 885){
+      alert (winner + "wins!");
+      if (winner === "Player One"){
+        this.p1Score++;
+        $('#p1-score').text(this.p1Score);
+      } else {
+        this.p2Score++;
+        $('#p2-score').text(this.p2Score);
+      }
+      this.winner = true;
+      console.log("p1: "+this.p1Score + "p2: " + this.p2Score + "winner: " + winner);
+    }
   }
 };
 
